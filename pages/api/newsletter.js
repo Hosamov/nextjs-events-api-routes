5fa63375827318caf6f8947851bb5ca370af9227
Hosamov <hosamov@hotmail.com>
2022-10-06 16:24:30 -0700

    setup mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,14 +1,24 @@
-function handler(req, res) {
-  if(req.method === 'POST') {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
     const userEmail = req.body.email;
 
-    if(!userEmail || !userEmail.includes('@')) {
-      res.status(422).json({message: 'Invalid email addres.'});
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({ message: 'Invalid email addres.' });
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({message: 'Signed up!'});
+    const client = await MongoClient.connect(
+      'mongodb+srv://testuser:gmd8SFa0JhcaRv3o@cluster0.mdrss.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
+
+    res.status(201).json({ message: 'Signed up!' });
   }
 }
 

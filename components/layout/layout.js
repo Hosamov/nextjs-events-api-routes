2c2e0abf55aa0703e23daf3e5413a9f95a2e01b6
Hosamov <hosamov@hotmail.com>
2022-10-22 19:47:51 -0700

    add notifications

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,25 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 function Layout(props) {
+  const notifictionCtx = useContext(NotificationContext);
+
+  const activeNotifiction = notifictionCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotifiction && (
+        <Notifiction
+          title={activeNotifiction.title}
+          message={activeNotifiction.message}
+          status={activeNotifiction.status}
+        />
+      )}
     </Fragment>
   );
 }
